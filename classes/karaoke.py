f7d89ea0c7bb3452d05f46a744cba417550feda4
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-08 13:47:31 +0100

    adds add room functinality and test~

--- classes/karaoke.py
+++ classes/karaoke.py
@@ -2,3 +2,6 @@ class Karaoke:
     def __init__(self, input_name):
         self.name = input_name
         self.room_list = []
+    
+    def add_karaoke_room(self, room_to_add):
+        self.room_list.append(room_to_add)
--- tests/karaoke_test.py
+++ tests/karaoke_test.py
@@ -1,11 +1,17 @@
 import unittest
 
 from classes.karaoke import Karaoke
+from classes.room import Room
 
 
 class TestKaraoke(unittest.TestCase):
     def setUp(self):
         self.karaoke1 = Karaoke("Venue 56")
+        self.room1 = Room("The tipsy gipsy")
     
     def test_karoke_name(self):
         self.assertEqual("Venue 56", self.karaoke1.name)
+    
+    def test_add_room(self):
+        self.karaoke1.add_karaoke_room(self.room1)
+        self.assertEqual(1, len(self.karaoke1.room_list))
