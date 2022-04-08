c5f3b5b72d156008510fffa892e3600e8cea2fb6
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-08 14:21:27 +0100

    adds wallet to Guests

--- classes/guest.py
+++ classes/guest.py
@@ -1,4 +1,5 @@
 class Guest:
-    def __init__(self, input_name, input_age):
+    def __init__(self, input_name, input_age, input_wallet):
         self.name = input_name
         self.age = input_age
+        self.wallet = input_wallet
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -5,11 +5,14 @@ from classes.guest import Guest
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        self.guest1 = Guest("Pikachu Gutierrez", 32)
-        self.guest2 = Guest("Eevee Costa", 25)
+        self.guest1 = Guest("Pikachu Gutierrez", 32, 100)
+        self.guest2 = Guest("Eevee Costa", 25, 50)
     
     def test_guest_name(self):
         self.assertEqual("Pikachu Gutierrez", self.guest1.name)
     
     def test_guest_age(self):
         self.assertEqual(25, self.guest2.age)
+
+    def test_guest_wallet(self):
+        self.assertEqual(100, self.guest1.wallet)
