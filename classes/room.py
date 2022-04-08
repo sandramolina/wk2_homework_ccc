c01c628577ebb0d38317b3363460ebe948af225d
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-08 12:14:22 +0100

    adds functionality to add a song to a room

--- classes/room.py
+++ classes/room.py
@@ -3,3 +3,6 @@ class Room:
         self.room_name = input_room_name
         self.guest_list = []
         self.song_list = []
+
+    def add_song_to_room(self, song_to_add):
+        self.song_list.append(song_to_add)
--- tests/room_test.py
+++ tests/room_test.py
@@ -7,6 +7,11 @@ class TestRoom(unittest.TestCase):
 
     def setUp(self):
         self.room1 = Room("The tipsy gipsy")
+        self.song_to_add1 = "Volare"
     
     def test_room_name(self):
         self.assertEqual("The tipsy gipsy", self.room1.room_name)
+
+    def test_song_add_song_to_room(self):
+        self.room1.add_song_to_room(self.song_to_add1)
+        self.assertEqual(1, len(self.room1.song_list))
