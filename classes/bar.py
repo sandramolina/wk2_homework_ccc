3e8c5a4d44061a1bb38ff8fd4c76d3a0b4a43949
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-09 14:29:04 +0100

    improves add_drinks_to_bar()

--- classes/bar.py
+++ classes/bar.py
@@ -3,9 +3,12 @@ class Bar:
         self.drinks = []
         self.bar_till = 0
 
-    def add_drinks_to_bar(self, drink_to_add, units_to_add):
-        self.drinks.append(drink_to_add)
-        drink_to_add.stock += units_to_add
+    def add_drinks_to_bar(self, drink_to_add, units_to_add): 
+        if drink_to_add not in self.drinks:
+            self.drinks.append(drink_to_add)   
+            drink_to_add.stock += units_to_add         
+        else:       
+            drink_to_add.stock += units_to_add
 
     def remove_drinks_from_bar(self, drink_to_remove, units_to_remove):
         self.drinks.remove(drink_to_remove)
