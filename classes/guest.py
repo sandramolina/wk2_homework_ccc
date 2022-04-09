54be17020168bb8c329de61558eaf88b510512cd
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-09 14:45:44 +0100

    adds buy_drink function to Guest class

--- classes/guest.py
+++ classes/guest.py
@@ -8,9 +8,15 @@ class Guest:
     def can_afford_entry_fee(self, karaoke):
         return self.wallet >= karaoke.entry_fee
     
+    def pay_entrance_fee(self):
+        pass
+
     def fav_song_played(self, room):
         quote = "Boooo"
         for song in room.playlist:
             if song == self.fav_song:
                 quote = "Yeah, this is my jam!!"
         return quote
+
+    def buy_drink(self, drink_to_buy, units_to_buy):
+        self.wallet -= drink_to_buy.price * units_to_buy
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,4 +1,5 @@
 import unittest
+from classes.drinks import Drinks
 
 from classes.guest import Guest
 from classes.song import Song
@@ -8,7 +9,10 @@ class TestGuest(unittest.TestCase):
     def setUp(self):
         self.guest1 = Guest("Pikachu Gutierrez", 32, 100, Song("Bohemian Rhapsody", "Queen", 6))
         self.guest2 = Guest("Eevee Costa", 25, 150, Song("Wannabe", "Spice Girls", 3))
-    
+
+        self.drink1 = Drinks("Beer", 3.4)
+        self.drink2 = Drinks("Cocktail", 5)
+        
     def test_guest_name(self):
         self.assertEqual("Pikachu Gutierrez", self.guest1.name)
     
@@ -20,3 +24,7 @@ class TestGuest(unittest.TestCase):
 
     def test_quest_fav_song(self):
         self.assertEqual("Wannabe", self.guest2.fav_song.song_name)
+    
+    def test_buy_drink(self):
+        self.guest1.buy_drink(self.drink1, 2)
+        self.assertEqual(93.2, self.guest1.wallet)
